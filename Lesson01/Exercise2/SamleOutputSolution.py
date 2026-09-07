017b0a002c2b80520a2892fffff5611cd8808cf3
Aniko Zsigri <ANIZ@ek.dk>
2026-09-07 16:07:04 +0200

    SamleOutput

---
+++ Lesson01/Exercise2/SamleOutputSolution.py
@@ -0,0 +1,9 @@
+overskrift = input("Skriv en overskrift: ")
+part1 = input("Skriv første sætning: ")
+part2 = input("Skriv anden sætning: ")
+part3 = input("Skriv tredje sætning: ")
+
+print()
+print("📢 " + overskrift)
+print(f"{part1} {part2} {part3}")
+print("🚀 Tak fordi du læste reklamen!")
